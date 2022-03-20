04f0237e43821139fe341bb86f06d25cde242098
jose-goncalez <josepgoncalez@usp.br>
2022-03-20 17:02:16 -0300

    Update colors code Prototipo 4

--- 2021/CodigoGrafico/Graficos no R/Prototipo codigo 4.R
+++ 2021/CodigoGrafico/Graficos no R/Prototipo codigo 4.R
@@ -1,7 +1,7 @@
-# Prot√≥tipo do c√≥digo para os gr√°ficos #
+# Prototipo do codigo para os graficos #
 #########################################################################################################################################
 
-# Limpa vari√°vel 
+# Limpa variavel 
 rm(list = ls())
 
 # Pacotes
@@ -15,7 +15,7 @@ library(svglite)
 
 
 
-# Importando gr√°fico e manipulando-os
+# Importando grafico e manipulando-os
 
 #Veteranos
 ex1 <- read_xlsx('Veteranos (final).xlsx', sheet=3)
@@ -44,8 +44,7 @@ get_pergunta_graph <- function(pergunta) {
   Bixo <- c(Bixo[1], 0, Bixo[2])
   Professor <- as.vector(get_media_grupo(Professores, pergunta))
 
-  Perguntas <- c("Quanto acha que \n deveria haver?", "Quanto acha \n que h√°?", "Qu√£o importante √© \n para o curso?")
-
+  Perguntas <- c("Quanto acha que \n deveria haver?", "Quanto acha \n que h·?", "Qu„o importante È \n para o curso?")
 
   df <- data.frame(Perguntas, Bixo, Veterano, Professor)
   df.m <- melt(df, id.vars='Perguntas')
@@ -62,28 +61,22 @@ get_pergunta_graph <- function(pergunta) {
   names(df.m)[names(df.m) == 'value'] <- 'Media'
   names(df.m)[names(df.m) == 'variable'] <- 'Grupos'
 
-
+  group.colors <- c(Veterano = "#32CD32", Bixo = "#FF6347", Professor ="#4169E1")
+  
   return(ggplot(df.m, aes(x = Perguntas, y = Media, fill = Grupos )) +
     geom_bar(position = position_dodge(0.7), stat="identity",width = 0.7) +
     geom_text(aes(label=Media), position=position_dodge(width=0.7), vjust = -0.2) +
-    ggtitle(paste(pergunta,':')) + theme(axis.text.x = element_text(size=12))
+    ggtitle(paste(pergunta,':')) + theme(axis.text.x = element_text(size=12)) 
+    + scale_fill_manual(values=group.colors)
     )
 }
 
 perguntas <- c(
-  '1. Com rela√ß√£o √† presen√ßa de ambientes f√≠sicos de estudo satisfat√≥rios fora de aula',
-  '2. Com rela√ß√£o √† compatibilidade do acervo da biblioteca com as exig√™ncias do curso'
+  '1. Com relaÁ„o ‡ presenÁa de ambientes fÌsicos de estudo satisfatÛrios fora de aula',
+  '2. Com relaÁ„o ‡ compatibilidade do acervo da biblioteca com as exigÍncias do curso'
 )
 for(pergunta in perguntas) {
-  # ggsave(
-  #   file=sprintf('%s.svg', pergunta),
-  #   plot=get_pergunta_graph(pergunta),
-  #   width=10,
-  #   height=6
-  # )
   get_pergunta_graph(pergunta)
   ggsave(paste(pergunta,'.svg'),device = svg,width=10, height=6)
-  #svg(filename =sprintf('%s.svg', pergunta))
-  #print(get_pergunta_graph(pergunta))
-  #dev.off()
+  
 }
